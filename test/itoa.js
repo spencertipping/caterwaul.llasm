6c988ffc4a355696d48b3f32cecd5fe3052f8623
Spencer Tipping <spencer@spencertipping.com>
2010-12-12 11:06:59 -0700

    Got the itoa example working (prints numbers backwards, but whatever). The problem was a direct move rather than an indirect one.

--- test/itoa.js
+++ test/itoa.js
@@ -12,6 +12,7 @@
 //     movl $4, %eax       ; write
 //     movl $1, %ebx       ; FD = 1 (stdout)
 //     movl %esp, %ecx     ; data is on the stack (little-endian)
+//     subl $3, %ecx
 //     movl $1, %edx       ; one byte
 //     int $0x80
 //     popl %edx
@@ -23,13 +24,13 @@
 //   int $0x80
 
 caterwaul.clone('std seq llasm.numerics llasm.elf llasm.asm llasm.struct')(function (require) {
-  var prelude = qas[xb8 /number.a2l],
+  var prelude = qas[x8b /b00_000101 /number.a2l],
       exit    = qas[x31db, xb8 /xl00000001, xcd80],
-      number  = ['number', caterwaul.llasm.struct()._unsigned_int('n')({n: 123456789})],
+      number  = qas[+number, ~caterwaul.llasm.struct()._unsigned_int('n')({n: 123456789})],
       loop    = qas[+top | x31d2, xbb /xl0000000a, xf7fb, x50, x81c2 /xl00000030, x52, xb8 /xl00000004, xbb /xl00000001, x89 /b11_100001
                          | xba /xl00000001, xcd80, x5a, x58, x85c0, x77 /top[1].r8],
 
-      bytes   = caterwaul.llasm.elf32.trivial_code(caterwaul.llasm.asm(seq[prelude + loop + exit + number], {base: 0x08048000 + 0x54}));
+      bytes   = caterwaul.llasm.elf32.trivial_code(caterwaul.llasm.asm(seq[prelude + loop + exit + number], {base: 0x08048054}));
 
   require('fs') /se.fs[fs.writeFile('test/itoa', new Buffer(bytes.length) /se.bs[seq[bytes *![bs[_i] = _]]], fn_[fs.chmodSync('test/itoa', 0700)])];
 })(require);
