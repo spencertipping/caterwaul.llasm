f25eec0c88ccd5cf416175766db6ff1e7c0110cf
Spencer Tipping <spencer@spencertipping.com>
2011-01-16 09:34:25 -0700

    Removed NASM data

--- data/hexify.js
+++
@@ -1,4 +0,0 @@
-var s = require('fs').readFileSync('insns.dat', 'ascii');
-var hexify = function (n) {return n < 16 ? '0' + n.toString(16) : n.toString(16)};
-process.stdout.write(s.replace(/\\x(..)/g,     function (_, n) {return hexify(parseInt(n, 16)) + ' '}).
-                       replace(/\\(\d{1,3})/g, function (_, n) {return hexify(parseInt(n, 8)) + ' '}));
